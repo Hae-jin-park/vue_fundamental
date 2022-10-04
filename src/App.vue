1ebc7295f18a89d577ca6e9142ff1469eb77fa01
Hae-jin-park <74353756+Hae-jin-park@users.noreply.github.com>
2022-10-04 15:38:31 +0900

    라이프 사이클
    created, mounted, unmounted

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <LifeCycle />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LifeCycle from "./components/LifeCycle.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    LifeCycle,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div ref="rDiv">{{ message }}</div>
+    <p>
+      <input
+        type="text"
+        class="form-control block w-96 m-auto px-3 py-1.5 my-9 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        id="exampleFormControlInput1"
+        @input="changeMessage"
+      />
+    </p>
+    <button
+      type="button"
+      @click="changeInputText"
+      class="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+    >
+      Primary
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      message: "Hello World!!",
+      inputText: "",
+    };
+  },
+
+  methods: {
+    changeMessage(e) {
+      this.inputText = e.target.value;
+    },
+
+    changeInputText() {
+      this.message = this.inputText;
+    },
+  },
+
+  beforeCreate() {
+    console.log("beforeCreate", this.message);
+  },
+
+  created() {
+    console.log("created", this.message);
+  },
+
+  beforeMount() {
+    console.log("beforeMount", this.message);
+  },
+
+  mounted() {
+    console.log("mounted", this.message, this.$refs.rDiv.innerText);
+  },
+
+  beforeUpdate() {
+    console.log("beforeUpdate", this.$refs.rDiv.innerText);
+  },
+
+  updated() {
+    console.log("updated", this.$refs.rDiv.innerText);
+  },
+
+  beforeUnmount() {
+    console.log("beforeUnmount", this.$refs.rDiv.innerText);
+  },
+
+  unmounted() {
+    console.log("unmounted", this.$refs.rDiv, this.inputText);
+  },
+};
+</script>
