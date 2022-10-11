537e8f86b3b61dff75ab52cf450d6cc1002427ad
Hae-jin-park <74353756+Hae-jin-park@users.noreply.github.com>
2022-10-11 17:11:44 +0900

    디자인 작업 완료 (feat, 주석작, tailwind css 가이드)

--- README.md
+++ README.md
@@ -1,24 +1,84 @@
 # vue_fundamental
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
+### Tailwind css install
+
+vue 사이드 프로젝트의 일관된 디자인을 쉽고 빠르게 하기 위해, Tailwind를 설치하고 사용하게 되었다.
+
+Tailwind 설치는 아주 간단하다. 여러 방법이 있지만 나는 Tailwind CLI 도구를 사용하였다.
+
+1. Tailwind CSS 설치
+
+npm 명령어로 설치했고, tailwind.config.js 파일을 만든다.
+
+npm install -D tailwindcss
+npx tailwindcss init
+
+2. 템플릿 경로 구성
+
+tailwind.config.js 에 모든 템플릿 파일에 대한 경로를 추가한다.
+
+/** @type {import('tailwindcss').Config} \*/
+module.exports = {
+content: ["./index.html", "./src/**/\*.{vue,js,ts,jsx,tsx}"],
+theme: {
+extend: {},
+},
+plugins: [],
+}
+
+3. css 파일을 하나 만들고 지시문 추가한다.
+
+/_ ./src/index.css _/
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+4. 지시문을 추가한 파일을 빌드한다.
+
+npx tailwindcss -i ./src/index.css -o ./src/output.css --watch
+
+5. 사용할 페이지에 컴파일된 파일을 추가한다.
+
+main.js
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import "./output.css"
+
+createApp(App).mount('#app')
+
+6. 클래스 추가하여 tailland 적용되었는지 확인한다.
+
+예시코드
+
+'<h1 class="text-3xl font-bold underline">{{ msg }}</h1>'
+
+7.서버 띄워 확인한다.
+
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -2,10 +2,10 @@
   <div id="main_container">
     <div class="grid grid-cols-1">
       <div
-        class="bg-white rounded-md m-10 px-4 py-4 border-2 border-emerald-600"
+        class="bg-white rounded-md m-2 sm:m-3 px-4 py-4 border-2 border-emerald-600"
       >
-        <h1 class="text-3xl font-bold">TODO List</h1>
-        <div>{{ address }}</div>
+        <!--Todo 신규 입력 부분 -->
+        <h1 class="text-3xl font-bold text-center">TODO List</h1>
         <div class="flex flex-row">
           <input
             type="text"
@@ -23,17 +23,19 @@
           </button>
         </div>
 
-        <div id="todoList-container">
+        <div>
+          <!--todoList 출력부 : ul, li 방식으로 출력-->
           <ul
-            class="bg-white rounded-lg border border-gray-200 w-full p-1 text-gray-900"
+            class="bg-white rounded-lg border border-gray-200 w-full p-1 m-1 md:m-0 text-gray-900"
           >
             <li
-              class="px-6 py-2 border-x border-y border-gray-200 w-full flex flex-row"
+              class="px-1 md:px-0 py-2 border-x border-y border-gray-200 w-full flex flex-row"
               v-for="(todo, index) in todoList"
               :key="index"
             >
               <div class="w-1/12">
-                <p class="hidden">{{ todo.renderTypeShow }}</p>
+                <!--NEW 표기부분-->
+                <p class="hidden">{{ todo.updateMode }}</p>
                 <img
                   class="h-6 mx-auto"
                   src="../assets/new_icon.png"
@@ -42,47 +44,52 @@
                   "
                 />
               </div>
-              <div class="w-11/12">
-                <p v-show="todo.renderTypeShow === 1">{{ todo.comment }}</p>
-                <p v-show="todo.renderTypeShow === 0">
+              <div class="w-full">
+                <!--todo Comment 부분 -->
+                <p v-show="todo.updateMode === false">{{ todo.comment }}</p>
+                <p v-show="todo.updateMode === true">
                   <input
                     type="text"
                     class="form-control block w-11/12 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    ref="input_ref_2"
+                    ref="input_ref2"
                     v-model="todo.comment"
+                    @keyup.esc="discardChange(index)"
+                    @keyup.enter="updateTodo(index)"
                   />
                 </p>
               </div>
-              <div class="w-1/12">
-                <p v-show="todo.renderTypeShow === 1">
+              <div class="w-3/12 sm:w-1/12">
+                <!--수정버튼-->
+                <p v-show="todo.updateMode === false">
                   <button
-                    class="form-control block text-base font-normal w-auto p-1 mx-auto text-white bg-orange-400 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-white focus:bg-orange-400 focus:border-blue-600 focus:outline-none"
+                    class="form-control block text-base font-normal w-full sm:w-full p-1 mx-auto text-white bg-orange-400 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-white focus:bg-orange-400 focus:border-blue-600 focus:outline-none"
                     @click="updateSwitch(index)"
                   >
                     <img class="h-4 mx-auto" src="../assets/edit.png" />
                   </button>
                 </p>
-                <p v-show="todo.renderTypeShow === 0">
+                <p v-show="todo.updateMode === true">
                   <button
-                    class="form-control block text-base font-normal w-auto p-1 mx-auto text-white bg-green-600 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-white focus:bg-green-600 focus:border-blue-600 focus:outline-none"
+                    class="form-control block text-base font-normal w-full sm:w-full p-1 mx-auto text-white bg-green-600 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-white focus:bg-green-600 focus:border-blue-600 focus:outline-none"
                     @click="updateTodo(index)"
                   >
                     <img class="h-4 mx-auto" src="../assets/check.png" />
                   </button>
                 </p>
               </div>
-              <div class="w-1/12">
-                <p v-show="todo.renderTypeShow === 1">
+              <div class="w-3/12 sm:w-1/12">
+                <!--삭제버튼-->
+                <p v-show="todo.updateMode === false">
                   <button
-                    class="form-control block text-base font-normal w-auto p-1 mx-auto text-white bg-red-600 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-white focus:bg-red-600 focus:border-blue-600 focus:outline-none"
+                    class="form-control block text-base font-normal w-full sm:w-full p-1 mx-auto text-white bg-red-600 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-white focus:bg-red-600 focus:border-blue-600 focus:outline-none"
                     @click="deleteTodo(index)"
                   >
                     <img class="h-4 mx-auto" src="../assets/bin.png" />
                   </button>
                 </p>
-                <p v-show="todo.renderTypeShow === 0">
+                <p v-show="todo.updateMode === true">
                   <button
-                    class="form-control block text-base font-normal w-auto p-1 mx-auto text-white bg-red-600 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-white focus:bg-red-600 focus:border-blue-600 focus:outline-none"
+                    class="form-control block text-base font-normal sm:w-full w-full p-1 mx-auto text-white bg-red-600 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-white focus:bg-red-600 focus:border-blue-600 focus:outline-none"
                     @click="discardChange(index)"
                   >
                     <img class="h-4 mx-auto" src="../assets/close.png" />
@@ -94,6 +101,7 @@
               class="px-6 py-2 border-x border-y border-gray-200 w-full"
               v-if="todoList.length === 0"
             >
+              <!--비어있을 경우 아래 메시지를 표시.-->
               comment를 입력하세요.
             </li>
           </ul>
@@ -109,53 +117,45 @@ export default {
     return {
       todoList: [],
       comment: "",
-      address: "",
-      ex_comment: "",
     };
   },
 
   methods: {
     apply() {
       if (!this.comment) {
+        //입력란이 비어있는 상태에서 이 함수를 실행할 경우 push작업을 하지 않는다.
         alert("내용을 입력하세요.");
         this.$refs.input_ref.focus();
         return;
       }
       this.todoList.push({
-        renderTypeShow: 1,
+        updateMode: false, //수정모드인 경우 true, 아니면 false.
         comment: this.comment,
         createdAt: new Date(),
-        address: this.address,
-        ex_comment: "",
+        ex_comment: "", //수정모드에서 취소버튼 누르면 기존값을 저장하는 임시변수.
       });
       this.todoList.sort(function (a, b) {
-        return a.createdAt - b.createdAt;
+        // 최근 등록된 일자 기준으로 a-b는 오름차순, b-a는 내림차순.
+        return b.createdAt - a.createdAt;
       });
-      this.comment = "";
-      console.log(this.todoList);
+      this.comment = ""; //새 글이 등록되면 기존 입력란을 비워준다.
     },
     deleteTodo(index) {
       if (
-        confirm("are you sure you want to delete No, " + index + " comment?")
+        confirm("Are you sure you want to delete No, " + index + " comment?")
       ) {
-        this.todoList.splice(index, 1);
+        this.todoList.splice(index, 1); //삭제작업.
       }
     },
     updateSwitch(index) {
-      this.todoList[index].renderTypeShow = 0;
-      this.todoList[index].ex_comment = this.todoList[index].comment;
-      console.log(this.ex_comment);
-
-      this.$refs.input_ref_2.focus();
+      this.todoList[index].updateMode = true; //updateMode가 true면 v-show를 통해 수정을 위한 textbox가 보여진다.
+      this.todoList[index].ex_comment = this.todoList[index].comment; //ex_comment 임시변수에 기존 내용 저장.
     },
     updateTodo(index) {
       if (
-        confirm("are you sure you want to update No, " + index + " comment?")
+        confirm("Are you sure you want to update No, " + index + " comment?")
       ) {
-        //this.comment = comment;
-        console.log(this.comment);
-        //this.todoList[index].comment = this.comment;
-        this.todoList[index].renderTypeShow = 1;
+        this.todoList[index].updateMode = false; //update작업이 끝났으니 mode는 false.
       }
     },
     discardChange(index) {
@@ -164,15 +164,15 @@ export default {
           "No, " + index + " comment's change will be discarded. Proceed?"
         )
       ) {
-        this.todoList[index].comment = this.todoList[index].ex_comment;
-        this.todoList[index].renderTypeShow = 1;
+        this.todoList[index].comment = this.todoList[index].ex_comment; //변경사항을 저장하지 않기에, ex_comment 임시변수의 값으로 되돌린다.
+        this.todoList[index].updateMode = false;
       }
     },
   },
   created() {
     setInterval(() => {
       this.todoList = this.todoList.map((todo) => todo);
-    }, 2000);
+    }, 1500);
   },
 };
 </script>
