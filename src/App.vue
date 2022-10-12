abfb6af95efbeb901bb23ef6448cd522158c4c47
Hae-jin-park <74353756+Hae-jin-park@users.noreply.github.com>
2022-10-12 14:18:31 +0900

    초기상태로 return

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-
-    <!--<ListRendering />-->
-
-    <!--<FormBinding />-->
-    <IfRenderingView />
   </div>
 </template>
 
 <script>
-//import ListRendering from "./components/ListRendering.vue";
-
-//import DataBinding01 from "./components/DataBinding01.vue";
-//dimport FormBinding from "./components/FormBinding.vue";
-import IfRenderingView from "./components/IfRenderingView.vue";
 export default {
   name: "App",
-  components: {
-    //DataBinding01,
-    //ListRendering,
-    //FormBinding,
-    IfRenderingView,
-  },
+  components: {},
 };
 </script>
 
